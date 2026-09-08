579f7a6243ff380092da8b519c2eb3e2572e4ec4
GavrishM <nanotechnology930@gmail.com>
2026-09-08 10:08:03 +0300

    я написал код, который выводит в консоли "кважаблябушка жаби-жаби"

---
+++ жабич/console1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("кважаблябушка жаби-жаби");
+        }
+    }
+}

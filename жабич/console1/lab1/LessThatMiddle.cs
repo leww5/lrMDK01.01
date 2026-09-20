a580d717c1f8611434ee2d607880c2c2b657d70f
Leww <lewwa51@gmail.com>
2026-09-21 02:14:41 +0300

    сделано первое задание первой лабороторной работы (2 вариант). нахождение кол-ва чисел, которые меньше среднего значения вводного массивареализована

---
+++ жабич/console1/lab1/LessThatMiddle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console1.lab1
+{
+    internal class LessThatMiddle
+    {
+        ///Задача 1. С клавиатуры ввести массив из n целых чисел. Определить количество элементов, меньших среднего значения массива.
+        public static void CountLessThanMiddle()
+        {
+            Console.WriteLine("Введите количество элементов массива (натуральное число)");
+            int n = input_natural_number.InputNaturalNumber();
+            int[] array = new int[n];
+            Console.WriteLine("Введите элементы массива:");
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            double average = array.Average();
+            int count = array.Count(x => x < average);
+            Console.WriteLine($"Количество элементов, меньших среднего значения массива ({average}): {count}");
+            Console.WriteLine("---");
+        }
+    }
+}
---
+++ жабич/console1/lab1/primeNumber.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console1.lab1
+{
+    internal class PrimeNumber
+    {
+    }
+}
